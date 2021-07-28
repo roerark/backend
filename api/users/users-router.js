aed67f463bdf55396dd9093ace9337db9a12b5b3
Alex Edwards <alexedwards@Alexs-MacBook-Pro.local>
2021-07-27 18:56:02 -0600

    i hate coding

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,43 +1,43 @@
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
-const tokenBuilder = require('../middleware/token-builder')
+const tokenBuilder = require("../middleware/token-builder");
 
 const { verifyLogin, checkUsernameFree } = require("./users-middleware.js");
 const Users = require("./users-model.js");
 
 router.get("/", (req, res, next) => {
   Users.findAll()
-    .then(users => {
+    .then((users) => {
       res.status(200).json(users);
     })
     .catch(next);
 });
 
 router.post("/login", verifyLogin, (req, res, next) => {
-  const {username} = req.body
-    
-  Users.findBy({username})
-  .then(([user]) => {
-      const token = tokenBuilder(user)
-      const user_id = user.user_id
+  const { username } = req.body;
+
+  Users.findBy({ username })
+    .then(([user]) => {
+      const token = tokenBuilder(user);
+      const user_id = user.user_id;
       res.json({
-          message: `Login successful`,
-          user_id,
-          token 
-      })
-  })
-  .catch(next)
+        message: `Login successfull`,
+        user_id,
+        token,
+      });
+    })
+    .catch(next);
 });
 
-router.post('/register', checkUsernameFree, (req, res, next) => {
-  const {username, password, location_id} = req.body
+router.post("/register", checkUsernameFree, (req, res, next) => {
+  const { username, password, location_id } = req.body;
   const hash = bcrypt.hashSync(password, 8);
 
-    Users.add({ username, password: hash, location_id })
-      .then((saved) => {
-        res.status(201).json(saved);
-      })
-      .catch(next);
-})
+  Users.add({ username, password: hash, location_id })
+    .then((saved) => {
+      res.status(201).json(saved);
+    })
+    .catch(next);
+});
 
 module.exports = router;
